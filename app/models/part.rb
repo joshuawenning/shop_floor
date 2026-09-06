f18bb75cd1e6aef818aee69de5e399ad6668867f
Joshua Wenning <joshuawenning@gmail.com>
2026-09-06 14:21:12 -0400

    Add validations to Part model and update tests/fixtures

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,2 +1,6 @@
 class Part < ApplicationRecord
+  validates :number, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :inventory_quantity,
+    numericality: { greater_than_or_equal_to: 0 }
 end
--- test/models/part_test.rb
+++ test/models/part_test.rb
@@ -1,7 +1,54 @@
 require "test_helper"
 
 class PartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a number" do
+    part = Part.new(
+      name: "Control Board Assembly",
+      inventory_quantity: 10
+    )
+
+    assert_not part.valid?
+    assert_includes part.errors[:number], "can't be blank"
+  end
+
+  test "requires a unique number" do
+    Part.create!(
+      number: "PCB-4421",
+      name: "Control Board Assembly",
+      inventory_quantity: 10
+    )
+
+    duplicate = Part.new(
+      number: "PCB-4421",
+      name: "Another Part",
+      inventory_quantity: 20
+    )
+
+    assert_not duplicate.valid?
+    assert_includes duplicate.errors[:number], "has already been taken"
+  end
+
+  test "requires a name" do
+    part = Part.new(
+      number: "PCB-4421",
+      inventory_quantity: 10
+    )
+
+    assert_not part.valid?
+    assert_includes part.errors[:name], "can't be blank"
+  end
+
+  test "inventory quantity cannot be negative" do
+    part = Part.new(
+      number: "PCB-4421",
+      name: "Control Board Assembly",
+      inventory_quantity: -1
+    )
+
+    assert_not part.valid?
+    assert_includes(
+      part.errors[:inventory_quantity],
+      "must be greater than or equal to 0"
+    )
+  end
 end
