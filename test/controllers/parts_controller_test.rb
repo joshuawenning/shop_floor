7087f72f01255bb51b4dbabb45046571644de4c9
Joshua Wenning <joshuawenning@gmail.com>
2026-09-06 15:07:56 -0400

    Add tests for valid and invalid creation

--- test/controllers/parts_controller_test.rb
+++ test/controllers/parts_controller_test.rb
@@ -20,4 +20,41 @@ class PartsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "h1", "PCB-4421"
   end
+
+  test "should get new" do
+    get new_part_url
+
+    assert_response :success
+  end
+
+  test "should create part" do
+    assert_difference("Part.count", 1) do
+      post parts_url, params: {
+        part: {
+          number: "INV-2200",
+          name: "Inverter Housing",
+          revision: "A",
+          inventory_quantity: 35
+        }
+      }
+    end
+
+    part = Part.find_by!(number: "INV-2200")
+
+    assert_redirected_to part_url(part)
+  end
+
+  test "should not create invalid part" do
+    assert_no_difference("Part.count") do
+      post parts_url, params: {
+        part: {
+          number: "",
+          name: "",
+          inventory_quantity: -1
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+  end
 end
