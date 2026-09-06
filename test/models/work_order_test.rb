6dd632a56e38ada7b0ca335246d0be8612b8c9b4
Joshua Wenning <joshuawenning@gmail.com>
2026-09-06 16:13:24 -0400

    Add tests for WorkOrder model

--- test/models/work_order_test.rb
+++ test/models/work_order_test.rb
@@ -1,7 +1,109 @@
 require "test_helper"
 
 class WorkOrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "belongs to a part" do
+    part = Part.create!(
+      number: "PCB-1000",
+      name: "Control Board",
+      inventory_quantity: 10
+    )
+
+    work_order = WorkOrder.create!(
+      number: "WO-1000",
+      part: part,
+      quantity: 50,
+      due_on: 1.week.from_now
+    )
+
+    assert_equal part, work_order.part
+    assert_includes part.work_orders, work_order
+  end
+
+  test "requires a number" do
+    part = Part.create!(
+      number: "PCB-1001",
+      name: "Control Board",
+      inventory_quantity: 10
+    )
+
+    work_order = WorkOrder.new(
+      part: part,
+      quantity: 50,
+      due_on: 1.week.from_now
+    )
+
+    assert_not work_order.valid?
+    assert_includes work_order.errors[:number], "can't be blank"
+  end
+
+  test "requires quantity greater than zero" do
+    part = Part.create!(
+      number: "PCB-1002",
+      name: "Control Board",
+      inventory_quantity: 10
+    )
+
+    work_order = WorkOrder.new(
+      number: "WO-1002",
+      part: part,
+      quantity: 0,
+      due_on: 1.week.from_now
+    )
+
+    assert_not work_order.valid?
+  end
+
+  test "is overdue when due date has passed" do
+    part = Part.create!(
+      number: "PCB-2000",
+      name: "Control Board",
+      inventory_quantity: 10
+    )
+
+    work_order = WorkOrder.new(
+      number: "WO-2000",
+      part: part,
+      quantity: 50,
+      due_on: 1.day.ago.to_date,
+      status: :active
+    )
+
+    assert work_order.overdue?
+  end
+
+  test "is not overdue when due date is in the future" do
+    part = Part.create!(
+      number: "PCB-2001",
+      name: "Control Board",
+      inventory_quantity: 10
+    )
+
+    work_order = WorkOrder.new(
+      number: "WO-2001",
+      part: part,
+      quantity: 50,
+      due_on: 1.day.from_now.to_date,
+      status: :active
+    )
+
+    assert_not work_order.overdue?
+  end
+
+  test "completed work order is not overdue" do
+    part = Part.create!(
+      number: "PCB-2002",
+      name: "Control Board",
+      inventory_quantity: 10
+    )
+
+    work_order = WorkOrder.new(
+      number: "WO-2002",
+      part: part,
+      quantity: 50,
+      due_on: 1.day.ago.to_date,
+      status: :completed
+    )
+
+    assert_not work_order.overdue?
+  end
 end
