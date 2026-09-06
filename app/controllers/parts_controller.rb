9f2207e2eb2e374f795b567e960099d3b7646b01
Joshua Wenning <joshuawenning@gmail.com>
2026-09-06 15:04:42 -0400

    Add part creation (new/create) and form view

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -6,4 +6,24 @@ class PartsController < ApplicationController
   def show
     @part = Part.find(params[:id])
   end
+
+  def new
+    @part = Part.new
+  end
+
+  def create
+    @part = Part.new(part_params)
+
+    if @part.save
+      redirect_to @part
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def part_params
+    params.expect(part: [ :number, :name, :revision, :inventory_quantity ])
+  end
 end
