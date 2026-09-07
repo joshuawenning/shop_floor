2ec9f552516d8fbc263f8c4fcb9768db4a7a1021
Joshua Wenning <joshuawenning@gmail.com>
2026-09-06 22:50:52 -0400

    Create unit tests for Operation model

--- test/models/operation_test.rb
+++ test/models/operation_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class OperationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "belongs to a work order" do
+    part = Part.create!(
+      number: "OP-PART-001",
+      name: "Test Part",
+      inventory_quantity: 10
+    )
+
+    work_order = WorkOrder.create!(
+      number: "OP-WO-001",
+      part: part,
+      quantity: 20,
+      due_on: 1.week.from_now
+    )
+
+    operation = Operation.create!(
+      work_order: work_order,
+      name: "Assembly",
+      position: 1
+    )
+
+    assert_equal work_order, operation.work_order
+    assert_includes work_order.operations, operation
+  end
+
+  test "requires a name" do
+    operation = Operation.new(position: 1)
+
+    assert_not operation.valid?
+    assert_includes operation.errors[:name], "can't be blank"
+  end
+
+  test "position must be greater than zero" do
+    operation = Operation.new(
+      name: "Assembly",
+      position: 0
+    )
+
+    assert_not operation.valid?
+  end
 end
