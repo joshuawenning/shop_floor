41acd5838a92a6d6b9d0d9017fcc80be8a2d5aa9
Joshua Wenning <joshuawenning@gmail.com>
2026-09-07 13:17:10 -0400

    Test dashboard controller to verify show action and active work orders

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get show" do
+    get root_url
+
+    assert_response :success
+    assert_select "h1", "Shop Floor"
+  end
+
+  test "shows active work orders" do
+    part = Part.create!(
+      number: "DASH-PART-001",
+      name: "Dashboard Part",
+      inventory_quantity: 10
+    )
+
+    work_order = WorkOrder.create!(
+      number: "WO-DASH-001",
+      part: part,
+      quantity: 50,
+      due_on: 1.week.from_now,
+      status: :active
+    )
+
+    get root_url
+
+    assert_response :success
+    assert_select "a", text: work_order.number
+  end
 end
